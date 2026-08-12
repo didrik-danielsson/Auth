bc8463259c4667a27ced88c8ae86bfceabe00473
didrik <dida0725@student.su.se>
2026-08-12 16:43:13 +0200

    working on jwt-implementation

--- auth-service/src/main/java/com/hifresh/auth_service/middleware/JwtAuthenticationFilter.java
+++ auth-service/src/main/java/com/hifresh/auth_service/middleware/JwtAuthenticationFilter.java
@@ -1,4 +1,50 @@
 package com.hifresh.auth_service.middleware;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import com.hifresh.auth_service.service.JwtService;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+        this.jwtService = new JwtService();
+    }
+
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return ;
+        }
+        try{
+            final String token = authHeader.substring(7);
+            final String useEmail = jwtService.extractUserName(token);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if(userEmail.isTokenValid())
+        }
+    }
 }
+
