690ac20d663232d7684443ed04c1fbf64486c926
didrik <dida0725@student.su.se>
2026-08-12 16:43:13 +0200

    working on jwt-implementation

---
+++ auth-service/src/main/java/com/hifresh/auth_service/service/JwtService.java
@@ -0,0 +1,55 @@
+package com.hifresh.auth_service.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${spring.security.jwt.secret}")
+    public String secretKey;
+
+    public String extractUserName(String token) {
+    return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claims != null ? claimsResolver.apply(claims) : null;
+    }
+    public Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    private Claims extractAllClaims(String token) {
+        try {
+
+            return Jwts.parser()
+                    .setSigningKey(getSignInKey())
+                    .setAllowedClockSkewSeconds(30)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }catch (JwtException e) {
+            return null;
+        }
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username != null && username.equals(userDetails.getUsername()));
+    }
+}
+
+
