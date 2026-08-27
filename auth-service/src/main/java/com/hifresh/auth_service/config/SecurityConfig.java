105daf13a0636b2798e47ae420ad19d24389112e
didrik <dida0725@student.su.se>
2026-08-27 12:51:11 +0200

    add setallowcredentials to cors

--- auth-service/src/main/java/com/hifresh/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/hifresh/auth_service/config/SecurityConfig.java
@@ -57,6 +57,8 @@ public class SecurityConfig {
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
+        configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
